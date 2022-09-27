4ee6def64c47e24a87ea0a44fe4f1b1b23a930fc
sinaehwang <godvv29756318@gmail.com>
2022-09-27 10:29:41 +0900

    Service 도입후 서비스 메소드를 기능이전함

--- src/main/java/com/hsn/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/hsn/exam/demo/controller/UsrArticleController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.hsn.exam.demo.service.ArticleService;
 import com.hsn.exam.demo.vo.Article;
 
 import lombok.AllArgsConstructor;
@@ -17,74 +18,24 @@ import lombok.NoArgsConstructor;
 
 @Controller
 public class UsrArticleController {
-	private int lastId;
-	private List<Article> articles;
+	
+	private ArticleService articleService;
 	
 	public UsrArticleController() {
-		lastId = 0;
-		articles = new ArrayList<>();
-		makeTestdata();
+		
+		articleService = new ArticleService();
+		
+		articleService.makeTestdata();
 	}
-	
-	//서비스 메소드
-	private void makeTestdata() {
-		
-		for(int i=0; i<10; i++) {
-			
-			String title = "제목"+i;
-			String body = "내용"+i;
-			
-			writeArticle(title,body);//게시물작성로직실행만 하면됨
-		}
-		
-	}
-	
-	private Article writeArticle(String title,String body) { //게시물작성로직이 중복되어 함수로 구현함
-
-		int id=lastId+1;
-		Article article = new Article(id,title,body);
-		
-		lastId = id;
-		
-		articles.add(article); //파라미터로 조립된 article을 리스트에 담는다.
-		
-		return article;
-	}
-	
-	private void deleteArticle(int id) {
-		 Article article = getArticle(id);
-		
-		articles.remove(article); //리스트에서 id로 찾은 게시글을 삭제한다.
-		
-	}
-	
-	private Article modifyArticle(int id,String title, String body) {
-		
-		Article article = getArticle(id);
-		article.setTitle(title);
-		article.setBody(body);
-		
-		return article;
-	}
-	
-	private Article getArticle(int id) {
-		
-		for(Article article:articles) {
-			if(article.getId() == id) {
-				return article;
-			}
-		}
-		
-		return null;
-	}
-
 
 	//액션 메소드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title,String body) { //제목과 내용을 파라미터로 받는 응답메소드 구현후,
 		
-		return writeArticle(title,body); //작성로직실행후 결과를 받기위해 doAdd를 리턴형태로 만듬
+		Article article = articleService.writeArticle(title,body); //작성로직실행후 결과를 받기위해 doAdd를 리턴형태로 만듬 
+		
+		return article;
 		
 	}
 	
@@ -92,6 +43,8 @@ public class UsrArticleController {
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
 	public List<Article> getArticles() { //리스트에 add된 article 리스트들을 보여주는 응답메소드가 된다.
+		
+		List<Article> articles = articleService.getArticles();
 		
 		return articles;
 
@@ -101,13 +54,13 @@ public class UsrArticleController {
 	@ResponseBody
 	public String doDelete(int id) {
 		
-		Article article = getArticle(id); //id를 기반으로 article을 찾는 함수
+		Article article = articleService.getArticle(id); //id를 기반으로 article을 찾는 함수
 		
 		if(article==null) {
 			return id+"번글은 존재하지 않습니다.";
 		}
 
-		deleteArticle(id); //찾은 id를 인자로 주고 실제 삭제하는 함수 실행시킴
+		articleService.deleteArticle(id); //찾은 id를 인자로 주고 실제 삭제하는 함수 실행시킴
 		
 		return id+"번글이 삭제되었습니다.";
 	}
@@ -116,13 +69,15 @@ public class UsrArticleController {
 	@ResponseBody
 	public Object doModify(int id,String title, String body) {//리턴타입을 String 과 Article 둘다 사용해야되기 때문에 Object타입을 사용함
 		
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article==null) {
 			return id+"번글은 존재하지 않습니다.";
 		}
 
-		return modifyArticle(id,title,body);
+		articleService.modifyArticle(id,title,body);
+		
+		return article;
 		
 	}
 	
@@ -130,7 +85,7 @@ public class UsrArticleController {
 	@ResponseBody
 	public Object getArticleAction(int id) {
 		
-		Article article = getArticle(id);
+		Article article = articleService.getArticle(id);
 		
 		if(article==null) {
 			return id+"번글은 존재하지 않습니다.";
---
+++ src/main/java/com/hsn/exam/demo/service/ArticleService.java
@@ -0,0 +1,75 @@
+package com.hsn.exam.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hsn.exam.demo.vo.Article;
+
+public class ArticleService {
+	
+	private int lastId;
+	private List<Article> articles;
+	
+	public ArticleService(){
+		lastId = 0;
+		articles = new ArrayList<>();
+	}
+
+	//서비스 메소드
+	public void makeTestdata() {
+		
+		for(int i=0; i<10; i++) {
+			
+			String title = "제목"+i;
+			String body = "내용"+i;
+			
+			writeArticle(title,body);//게시물작성로직실행만 하면됨
+		}
+		
+	}
+	
+	public Article writeArticle(String title,String body) { //게시물작성로직이 중복되어 함수로 구현함
+
+		int id=lastId+1;
+		Article article = new Article(id,title,body);
+		
+		lastId = id;
+		
+		articles.add(article); //파라미터로 조립된 article을 리스트에 담는다.
+		
+		return article;
+	}
+	
+	public void deleteArticle(int id) {
+		 Article article = getArticle(id);
+		
+		articles.remove(article); //리스트에서 id로 찾은 게시글을 삭제한다.
+		
+	}
+	
+	public Article modifyArticle(int id,String title, String body) {
+		
+		Article article = getArticle(id);
+		article.setTitle(title);
+		article.setBody(body);
+		
+		return article;
+	}
+	
+	public Article getArticle(int id) {
+		
+		for(Article article:articles) {
+			if(article.getId() == id) {
+				return article;
+			}
+		}
+		
+		return null;
+	}
+
+	public List<Article> getArticles() {
+		
+		return articles;
+	}
+
+}
