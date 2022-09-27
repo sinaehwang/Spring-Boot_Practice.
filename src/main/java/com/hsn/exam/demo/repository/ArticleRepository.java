520cf1339a46a1688c9a155624f20c69af255544
sinaehwang <godvv29756318@gmail.com>
2022-09-27 22:18:03 +0900

    SQL쿼리문을 xml파일로 분리후 자바스크립트로 재구현

--- src/main/java/com/hsn/exam/demo/repository/ArticleRepository.java
+++ src/main/java/com/hsn/exam/demo/repository/ArticleRepository.java
@@ -2,35 +2,22 @@ package com.hsn.exam.demo.repository;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 import com.hsn.exam.demo.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
 
-	@Insert("INSERT INTO article SET regDate = NOW(), updateDate = NOW(), title = #{title}, `body` =#{body}")
 	public void writeArticle(String title,String body); //리턴타입이 없기때문에 id를 생성할수없어 타입오류가 발생됨
-	// INSER INTO article SET regDate = NOW(), updateDate = NOW(), title = ?, body =?
 	
-	@Select("SELECT * FROM article WHERE id = #{id}")
 	public Article getArticle(int id);
 	
-	@Delete("DELETE FROM article WHERE id = #{id}")
 	public void deleteArticle(int id);
-	// DELETE FROM article WHERE id = ?
-	@Update("UPDATE article SET regDate = NOW(), updateDate = NOW(), title = #{title} , `body` = #{body} WHERE id = #{id}")
-	public void modifyArticle(int id,String title, String body); 
-	// UPDATE article SET regDate = NOW(), updateDate = NOW(), title = ? , body = ?WHERE id = ?
-	@Select("SELECT * FROM article ORDER BY id DESC")
-	public List<Article> getArticles();
-	// SELECT * FROM article ORDER BY id DESC
 
-	@Select("SELECT LAST_INSERT_ID()") //마지막 게시글의 id를 구해오는 쿼리문
+	public void modifyArticle(int id,String title, String body); 
+
+	public List<Article> getArticles();
+
 	public int getLastInsertId();
 }
