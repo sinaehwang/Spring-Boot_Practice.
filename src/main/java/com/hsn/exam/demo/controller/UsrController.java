3eee9835847f20c09bc27939ab4a10c5d9046b7b
sinaehwang <godvv29756318@gmail.com>
2022-10-04 22:50:48 +0900

    pom에 JSP추가하기& usr/home/main접속시 jsp파일 작동시키기

--- src/main/java/com/hsn/exam/demo/controller/UsrController.java
+++ src/main/java/com/hsn/exam/demo/controller/UsrController.java
@@ -3,13 +3,19 @@ package com.hsn.exam.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class UsrController {
-	@RequestMapping("/usr/home/getString")
-	@ResponseBody
-	public String getString() {
-		return "hi";
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
+		//return "/WEB-INF/jsp/usr/home/main.jsp"; //yml에서 prefix랑suffix 시작과 끝주소가 써져있기때문에 생략가능
+		return "/usr/home/main"; 
+	}
+	
+	@RequestMapping("/") //자동응답처럼 /usr/home/main이 실행된다
+	public String showRoot() {
+		return "redirect:/usr/home/main";
 	}
 
 }
---
+++ src/main/webapp/WEB-INF/jsp/usr/home/main.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>메인</title>
+</head>
+<body>
+	<h1>메인페이지</h1>
+	<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus asperiores sunt at iure itaque nisi totam laborum nostrum ab soluta facilis sapiente ipsam autem in incidunt ut quis iste porro?</div>
+</body>
+</html>
